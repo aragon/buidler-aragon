143c8293faefa04a123dcfaf81a7e49e67fc5a76
Gabriel <gabrielpk.18@gmail.com>
2020-02-04 14:07:24 -0300

    Add builder configuration for buidlerevm

--- buidler.config.js
+++ buidler.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  network: {
+    buidlerevm: {
+      chainId: 1,
+      accounts: {
+        mnemonic:
+          'explain tackle mirror kit van hammer degree position ginger unfair soup bonus'
+      },
+      blockGasLimit: 50e6
+    }
+  },
   solc: {
     version: '0.4.24'
   }
--- test/projects/counter/buidler.config.ts
+++ test/projects/counter/buidler.config.ts
@@ -9,8 +9,21 @@ import { loadPluginFile } from '@nomiclabs/buidler/plugins-testing'
 loadPluginFile(__dirname + '/../../../src/index')
 
 const config: BuidlerConfig = {
-  defaultNetwork: 'localhost',
+  defaultNetwork: 'buidlerevm',
   networks: {
+    buidlerevm: {
+      accounts: [
+        {
+          privateKey: '0xa8a54b2d8197bc0b19bb8a084031be71835580a01e70a45a13babd16c9bc1563',
+          balance: '10000000000000000000000',
+        },
+        {
+          privateKey: '0xce8e3bda3b44269c147747a373646393b1504bfcbb73fc9564f5d753d8116608',
+          balance: '10000000000000000000000',
+        }
+      ],
+      blockGasLimit: 50e6
+    },
     localhost: {
       url: 'http://localhost:8545',
       accounts: [
