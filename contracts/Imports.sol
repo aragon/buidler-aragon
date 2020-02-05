710dbc7ac1a07ae0c13c3139e5a1f50c4e3f2093
Gabriel <gabrielpk.18@gmail.com>
2020-02-05 01:32:32 -0300

    Add PublicResolver import

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -10,11 +10,7 @@ import "@aragon/os/contracts/apm/Repo.sol";
 import "@aragon/os/contracts/ens/ENSSubdomainRegistrar.sol";
 import "@aragon/os/contracts/lib/ens/ENS.sol";
 import "@aragon/os/contracts/lib/ens/AbstractENS.sol";
-
-// import "@aragon/id/contracts/FIFSResolvingRegistrar.sol";
-
-// import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
-
+import "@aragon/os/contracts/lib/ens/PublicResolver.sol";
 
 contract Imports {
     // solium-disable-previous-line no-empty-blocks
