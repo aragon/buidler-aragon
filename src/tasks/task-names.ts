7d1e99eb156a7861cd3948c04c23ddfaab3569b7
ajsantander <palebluedot@gmail.com>
2020-02-21 12:18:30 -0300

    Import all Buidler builtin task names

--- src/tasks/task-names.ts
+++ src/tasks/task-names.ts
@@ -1,5 +1,6 @@
+
 // Aragon plugin tasks.
 export const TASK_START = 'start'
 
 // Buidler built-in tasks.
-export const TASK_COMPILE = 'compile'
+export * from '@nomiclabs/buidler/builtin-tasks/task-names'
