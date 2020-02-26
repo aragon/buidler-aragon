1f64ed47cdab8104b6ec514f801c21cfe8e055ab
dapplion <guillem.lion.benosa@gmail.com>
2020-02-26 22:18:40 +0800

    Document non-obvious functionality

--- src/utils/onExit.ts
+++ src/utils/onExit.ts
@@ -1,3 +1,10 @@
+/**
+ * Register a callback that will be called when the process is interrupted.
+ * Should be used by watchers to clean their fs.watch instances
+ * @param callback ```
+ * () => { watcher.close() }
+ * ```
+ */
 export default function onExit(callback: () => void): void {
   process.on('exit', callback)
   process.on('SIGINT', callback)
