1f0320507a71fb0bc19452bfd680589ee5683fa4
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-03-25 13:43:27 -0300

    Review: Update abi order

--- src/utils/apm/abis.ts
+++ src/utils/apm/abis.ts
@@ -5,9 +5,9 @@ export const apmRegistryAbi = [
 
 export const repoAbi = [
   'function newVersion(uint16[3] _newSemanticVersion, address _contractAddress, bytes _contentURI)',
-  'event NewVersion(uint256 versionId, uint16[3] semanticVersion)',
   'function getLatest() public view returns (uint16[3] semanticVersion, address contractAddress, bytes contentURI)',
-  'function getBySemanticVersion(uint16[3] _semanticVersion) public view returns (uint16[3] semanticVersion, address contractAddress, bytes contentURI)'
+  'function getBySemanticVersion(uint16[3] _semanticVersion) public view returns (uint16[3] semanticVersion, address contractAddress, bytes contentURI)',
+  'event NewVersion(uint256 versionId, uint16[3] semanticVersion)'
 ]
 
 export const appStorageAbi = ['function kernel() public view returns (IKernel)']
