7225fc4e7baef3a0fa9a002944ef6f7d568f310b
Gabriel <gabrielpk.18@gmail.com>
2020-03-27 17:06:05 -0300

    Add missing import

--- src/params.ts
+++ src/params.ts
@@ -1,3 +1,5 @@
+import { AragonConfig } from './types'
+
 // Standard expected Aragon file paths
 export const artifactName = 'artifact.json'
 export const manifestName = 'manifest.json'
@@ -91,6 +93,7 @@ export const etherscanSupportedChainIds = new Set([
   // Kovan
   42
 ])
+
 export const defaultAragonConfig: AragonConfig = {
   appServePort: 8001,
   clientServePort: 3000,
