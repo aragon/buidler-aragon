13eb02d83bb168d1a1ff6b75382b360a58ad9a54
Gabriel Garcia <gabrielpk.18@gmail.com>
2020-06-16 19:52:36 -0300

    Update client version with fixed worker

--- src/tasks/start/client/aragon-client.ts
+++ src/tasks/start/client/aragon-client.ts
@@ -8,7 +8,7 @@ import { logFront } from '~/src/ui/logger'
 import { ensureDir, writeJson, pathExists, remove } from '~/src/utils/fsUtils'
 
 const defaultRepo = 'https://github.com/0xGabi/aragon'
-const defaultVersion = '456815dbc991a6d08904e8ff462bdb1f4d42dc6c'
+const defaultVersion = '8241ccdfd69de528500af4d8cc6ad59e00c5d41a'
 const aragonBaseDir: string = path.join(os.homedir(), '.aragon')
 
 export async function installAragonClientIfNeeded(
